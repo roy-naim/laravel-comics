8c8d9f3845768bd623a88457da2dfc4fd469aebd
Roy <b.roynaim@gmail.com>
2021-05-16 03:15:39 +0300

    ciclo array per comics @foreach

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,14 @@
 @extends('layouts.app')
 
 @section('main')
-  <p>Home Party</p>
+  <div class="container-flex">
+    @foreach ($comics as $comic)
+      <div class="card">
+        <div class="thumb-img">
+          <img src="{{$comic['thumb']}}" alt="thumb-img">
+        </div>
+        <h4>{{$comic['series']}}</h4>
+      </div>
+    @endforeach
+  </div>
 @endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,8 +1,39 @@
 <header>
-  <ul>
-    <li>
-      <a href="/info">Info</a>
-    </li>
-    <li class="ciao">ciao</li>
-  </ul>
+  <nav>
+    <ul>
+      <li>
+        <a href="/info">CHARACTERS</a>
+      </li>
+      <li>
+        <a href="/info">COMICS</a>
+      </li>
+      <li>
+        <a href="/info">MOVIES</a>
+      </li>
+      <li>
+        <a href="/info">TV</a>
+      </li>
+      <li>
+        <a href="/info">GAMES</a>
+      </li>
+      <li>
+        <a href="/info">COLLECTIBLES</a>
+      </li>
+      <li>
+        <a href="/info">VIDEOS</a>
+      </li>
+      <li>
+        <a href="/info">FANS</a>
+      </li>
+      <li>
+        <a href="/info">NEWS</a>
+      </li>
+      <li>
+        <a href="/info">SHOP</a>
+      </li>
+    </ul>
+  </nav>
+  <div class="jumbo">
+
+  </div>
 </header>
